511f6ff46737a056ad03c75692c977082875909b
LisGein <lisgein47@gmail.com>
2017-06-06 10:31:45 +0300

    russian translate

---
+++ languages/Diploma_ru.ts
@@ -0,0 +1,149 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../datasetWidget/datasetView.cpp" line="9"/>
+        <source>Add column</source>
+        <translation>Добавить колонку</translation>
+    </message>
+    <message>
+        <location filename="../datasetWidget/datasetView.cpp" line="10"/>
+        <source>Add row</source>
+        <translation>Добавить строку</translation>
+    </message>
+    <message>
+        <location filename="../datasetWidget/datasetView.cpp" line="11"/>
+        <source>Delete this column</source>
+        <translation>Удалить эту колонку </translation>
+    </message>
+    <message>
+        <location filename="../datasetWidget/datasetView.cpp" line="12"/>
+        <source>Delete this row</source>
+        <translation>Удалить эту строку</translation>
+    </message>
+    <message>
+        <location filename="../datasetWidget/datasetView.cpp" line="13"/>
+        <source>Rename this column</source>
+        <translation>Переименовать эту колонку</translation>
+    </message>
+    <message>
+        <location filename="../datasetWidget/datasetView.cpp" line="69"/>
+        <location filename="../datasetWidget/datasetView.cpp" line="90"/>
+        <source>Column name</source>
+        <translation>Имя колонки</translation>
+    </message>
+    <message>
+        <location filename="../datasetWidget/datasetView.cpp" line="69"/>
+        <location filename="../datasetWidget/datasetView.cpp" line="90"/>
+        <source>Input column name:</source>
+        <translation>Введите имя колонки:</translation>
+    </message>
+    <message>
+        <location filename="../mainWindow/plotController.cpp" line="17"/>
+        <source>Change graphic</source>
+        <translation>Поменять график</translation>
+    </message>
+    <message>
+        <location filename="../mainWindow/mainWindow.cpp" line="85"/>
+        <source>Open File</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="../mainWindow/mainWindow.cpp" line="108"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../datasetWidget/datasetWidget.cpp" line="11"/>
+        <source>Load from Excel</source>
+        <translation>Загрузить таблицу из Excel</translation>
+    </message>
+    <message>
+        <location filename="../datasetWidget/datasetWidget.cpp" line="15"/>
+        <source>Load from Tsv</source>
+        <translation>Загрузить Tsv таблицу</translation>
+    </message>
+    <message>
+        <location filename="../datasetWidget/datasetWidget.cpp" line="23"/>
+        <source>Description of the document</source>
+        <translation>Описание документа</translation>
+    </message>
+    <message>
+        <location filename="../dataPlotWidget/abstractDataPlot.cpp" line="10"/>
+        <source>Axis x:</source>
+        <translation>Ось x:</translation>
+    </message>
+    <message>
+        <location filename="../dataPlotWidget/abstractDataPlot.cpp" line="11"/>
+        <source>Axis y:</source>
+        <translation>Ось y:</translation>
+    </message>
+</context>
+<context>
+    <name>DataPlotWidget</name>
+    <message>
+        <location filename="../dataPlotWidget/dataPlotWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExperimentWidget</name>
+    <message>
+        <location filename="../experimentWidget/experimentWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../experimentWidget/experimentWidget.ui" line="24"/>
+        <source>Response</source>
+        <translation>Ответы</translation>
+    </message>
+    <message>
+        <location filename="../experimentWidget/experimentWidget.ui" line="36"/>
+        <source>Feature</source>
+        <translation>Фичи</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainWindow/mainWindow.ui" line="14"/>
+        <source>Main Window</source>
+        <translation>Главное окно</translation>
+    </message>
+    <message>
+        <location filename="../mainWindow/mainWindow.ui" line="44"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../mainWindow/mainWindow.ui" line="51"/>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+    <message>
+        <location filename="../mainWindow/mainWindow.ui" line="60"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="../mainWindow/mainWindow.ui" line="65"/>
+        <location filename="../mainWindow/mainWindow.ui" line="75"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../mainWindow/mainWindow.ui" line="70"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../mainWindow/mainWindow.ui" line="80"/>
+        <source>Load Tsv table</source>
+        <translation>Загрузить Tsv таблицу</translation>
+    </message>
+</context>
+</TS>
