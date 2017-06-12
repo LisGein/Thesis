3d505fd0a91d076ac64335a737d780d19117d3f0
LisGein <lisgein47@gmail.com>
2017-06-12 16:40:53 +0300

    fixed compilation on windows(arma)

--- cmakemodules/script_findlibs.cmake
+++ cmakemodules/script_findlibs.cmake
@@ -5,7 +5,12 @@ include(cmakemodules/script_libboost.cmake REQUIRED)
 include(cmakemodules/script_libexpat.cmake REQUIRED)
 include(cmakemodules/script_libxls.cmake REQUIRED)
 
-find_package( Armadillo REQUIRED )
+if (WIN32)
+	find_package( Armadillo PATHS "$ENV{ProgramFiles}/Armadillo/share/Armadillo/CMake" REQUIRED)
+else()
+	find_package( Armadillo REQUIRED)
+endif()
+
 INCLUDE_DIRECTORIES(${Armadillo_INCLUDE_DIR} )
 
 
